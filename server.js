c2fa448f227692589bdc3af6062fd5abb98a9f19
Val <kalenikvalentin@yahoo.com>
2021-09-27 14:47:33 -0500

    section 2

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const connectDb = require('./config/db');
 const app = express();
+
+// Connect Database
+connectDb();
+
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('API Running'));
-app.listen(PORT, _ => console.log(`Server started on port ${PORT}`));
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+
+app.listen(PORT, (_) => console.log(`Server started on port ${PORT}`));
