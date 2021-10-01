5677fb5251f70881cd6dc5fdbca7b2733cb8c584
Val <kalenikvalentin@yahoo.com>
2021-10-01 15:09:03 -0500

    section 4

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ connectDb();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5005;
 
 app.get('/', (req, res) => res.send('API Running'));
 
